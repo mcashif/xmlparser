d09bca4375c4d8e1e5d2922c7ca40fb71b805b40
mcashif <mcashif@hotmail.com>
2016-06-23 06:01:12 +0500

    Second Commit Working

--- xp/views.py
+++ xp/views.py
@@ -125,10 +125,10 @@ def cleanXMLLevel1(path):
 def dumpDB():
     db_User_Name = 'xmlparse'
     DB_User_Password = 'test1234'
-    DB_Name = 'xmlparse$xmlparser.xp_xmldata'
+    DB_Name = 'xmlparse$xmlparser'
     backupDir = '/home/xmlparse/xmlparser/media'
 
-    mysqldump_cmd = "mysqldump -u " + db_User_Name + " --password='" + DB_User_Password + "' -h xmlparse.mysql.pythonanywhere-services.com --databases '" + DB_Name + "' > " + backupDir + "/" + "dump.sql"
+    mysqldump_cmd = "mysqldump -u " + db_User_Name + " --password='" + DB_User_Password + "' -h xmlparse.mysql.pythonanywhere-services.com --databases '" + DB_Name + "'" + " xp_xmldata" +" > " + backupDir + "/" + "dump.sql"
     os.system(mysqldump_cmd)
 
 #Helper Function Remove Directory
